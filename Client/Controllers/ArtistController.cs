19f59df587bffcac2f729330898b034104ac7b2c
Jwan Khalaf <jwan.khalaf@outlook.com>
2020-03-19 16:55:18 +0000

    add ability to add a new artist

---
+++ Client/Controllers/ArtistController.cs
@@ -0,0 +1,50 @@
+﻿namespace Client.Controllers
+{
+  using Microsoft.AspNetCore.Mvc;
+  using Services;
+  using System;
+  using System.Collections.Generic;
+  using System.Threading.Tasks;
+  using ViewModels.Artist;
+
+  public class ArtistController : Controller
+  {
+    private readonly IArtistService artistService;
+
+    public ArtistController(IArtistService artistService)
+    {
+      this.artistService = artistService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+      ViewModels.Artist.Index viewModel = new ViewModels.Artist.Index();
+
+      IEnumerable<Item> artists = await artistService.GetArtistsAsync();
+
+      viewModel.Artists = artists;
+
+      return View(viewModel);
+    }
+
+    public async Task<IActionResult> Details(int id)
+    {
+      Item artist = await artistService.GetArtistByIdAsync(id);
+
+      return View(artist);
+    }
+
+    public IActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Item viewModel)
+    {
+      int artistId = await artistService.AddNewArtistAsync(viewModel);
+
+      return RedirectToAction("Details", new { id = artistId });
+    }
+  }
+}
---
+++ Services/IArtistService.cs
@@ -0,0 +1,15 @@
+﻿namespace Services
+{
+  using System.Collections.Generic;
+  using System.Threading.Tasks;
+  using ViewModels.Artist;
+
+  public interface IArtistService
+  {
+    Task<IEnumerable<Item>> GetArtistsAsync();
+    
+    Task<Item> GetArtistByIdAsync(int id);
+
+    Task<int> AddNewArtistAsync(Item artist);
+  }
+}
