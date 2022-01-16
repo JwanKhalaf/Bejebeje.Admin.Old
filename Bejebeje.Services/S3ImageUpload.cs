c265538149ad0dfce15ca230327794f4cc365d3d
Jwan Khalaf <jwan.khalaf@outlook.com>
2022-01-16 20:35:46 +0000

    get the s3 image upload function working

--- Bejebeje.Services/S3ImageUpload.cs
+++ Bejebeje.Services/S3ImageUpload.cs
@@ -1,7 +1,9 @@
 namespace Bejebeje.Services;
 
+using System;
 using System.IO;
 using System.Threading.Tasks;
+using Amazon;
 using Amazon.Runtime;
 using Amazon.S3;
 using Amazon.S3.Transfer;
@@ -20,28 +22,36 @@ public class S3ImageUploadService : IS3ImageUploadService
   
   public async Task UploadImageToS3Async(IFormFile file)
   {
-    BasicAWSCredentials credentials = new BasicAWSCredentials(_awsOptions.AccessKey, _awsOptions.AccessSecret);
-    
-    AmazonS3Config config = new AmazonS3Config
+    try
     {
-      RegionEndpoint = Amazon.RegionEndpoint.EUNorth1
-    };
+      BasicAWSCredentials credentials = new BasicAWSCredentials(_awsOptions.AccessKey, _awsOptions.AccessSecret);
     
-    using AmazonS3Client client = new AmazonS3Client(credentials, config);
+      AmazonS3Config config = new AmazonS3Config
+      {
+        RegionEndpoint = Amazon.RegionEndpoint.EUWest2
+      };
     
-    await using MemoryStream newMemoryStream = new MemoryStream();
-    file.CopyTo(newMemoryStream);
+      using AmazonS3Client client = new AmazonS3Client(credentials, config);
+    
+      await using MemoryStream newMemoryStream = new MemoryStream();
+      file.CopyTo(newMemoryStream);
 
-    TransferUtilityUploadRequest uploadRequest = new TransferUtilityUploadRequest
+      TransferUtilityUploadRequest uploadRequest = new TransferUtilityUploadRequest
+      {
+        InputStream = newMemoryStream,
+        Key = file.FileName,
+        BucketName = _awsOptions.BucketName,
+        CannedACL = S3CannedACL.PublicRead
+      };
+
+      TransferUtility fileTransferUtility = new TransferUtility(client);
+    
+      await fileTransferUtility.UploadAsync(uploadRequest);
+    }
+    catch (Exception e)
     {
-      InputStream = newMemoryStream,
-      Key = file.FileName,
-      BucketName = _awsOptions.BucketName,
-      CannedACL = S3CannedACL.PublicRead
-    };
-
-    TransferUtility fileTransferUtility = new TransferUtility(client);
-    
-    await fileTransferUtility.UploadAsync(uploadRequest);
+      Console.WriteLine(e);
+      throw;
+    }
   }
 }
